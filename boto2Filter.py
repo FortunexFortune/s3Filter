74c4b301ef4d67e1066d5cf8c7a7c8ea676b9a82
Fortune Osadolor <33355125+FortunexFortune@users.noreply.github.com>
2019-07-05 15:07:54 +0100

    Create boto2Filter.py

---
+++ boto2Filter.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python3
+from boto.s3.connection import S3Connection
+import boto
+from datetime import datetime
+
+def days_between(d1, d2):
+    d1 = datetime.strptime(d1, "%Y-%m-%d %H:%M:%S")
+    d2 = datetime.strptime(d2, "%Y-%m-%d %H:%M:%S")
+    return abs((d2 - d1).days)
+
+def dateFormatter(unoformattedDate):
+    year = unoformattedDate[:5]
+    month = unoformattedDate[5:8]
+    day = unoformattedDate[8:10]
+    hour = unoformattedDate[12:19]
+    date = year+month+day+" "+hour
+    return date
+
+
+aws_connection = S3Connection( host = 's3.eu-west-1.amazonaws.com')
+bucket = aws_connection.get_bucket( 'kds-uav-dev', validate=False)
+uavs = bucket.list()
+uavsOfIntrest = []
+
+for uav in uavs:
+    lastModified = uav.__getattribute__('last_modified')
+    date = dateFormatter(lastModified)
+    currentDate = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    diff = days_between(date, currentDate)
+
+    #print(lastModified)
+
+    
+    if diff <= 7:
+        uavsOfIntrest.append(uav)
+        print( date , " date modified - today's date = " , diff )
+
+print(uavsOfIntrest)
+    
